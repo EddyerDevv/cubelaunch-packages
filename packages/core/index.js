48f2227719f53ebaf52cdb595a08a24efae9bd66
EddyerDevv <urbinaramalazo1986@email.com>
2023-10-26 16:07:59 -0600

    Added Package Core

---
+++ packages/core/index.js
@@ -0,0 +1,19 @@
+/**
+ * El paquete principal para iniciar Minecraft.
+ * Proporciona la función {@link Version.parse} para analizar la versión de Minecraft,
+ * y la función {@link launch} para iniciar el juego.
+ *
+ * @author EddyerDevv - Linsx Studios
+ * @license MIT
+ * @packageDocumentation
+ * @module cubelaunch-core
+ */
+
+module.exports = {
+  ...require('./files/platform'),
+  ...require('./files/folder'),
+  ...require('./files/diagnose'),
+  checksum: require('./files/utils').checksum,
+  // ...require('./launch'),
+  // ...require('./version'),
+};
